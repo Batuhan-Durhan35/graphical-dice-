03b2d7ec7af8bb5785e657736fe460f276716679
Batuhan-Durhan35 <71413827+Batuhan-Durhan35@users.noreply.github.com>
2020-09-23 17:53:01 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+let number = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    number = randint(1, 6)
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (number == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
